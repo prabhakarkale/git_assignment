8c768e96e5ee476a4d812c8e70ce7f02be4705a2
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-02 16:32:07 +0530

    added DivideOperation.java file in feature-003 branch

---
+++ DivideOperation.java
@@ -0,0 +1,12 @@
+class DivideOperation
+{
+    public static void main(String args[])
+    {
+        int a = 10, b = 2;
+        System.out.println("Division : " + divideOp(a,b));
+    }
+    float divideOp(int a, int b)
+    {
+        reutrn a/b;
+    }
+}

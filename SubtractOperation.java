19f113a48cfb34811d5b1623d0751a8aab7ca88d
prabhakarkale <prabhakar.kale@cuelogic.com>
2021-06-02 12:47:50 +0530

    added SubtractOperation.java file and pushing branch feature-001

---
+++ SubtractOperation.java
@@ -0,0 +1,12 @@
+class SubtractOperation
+{
+    public static void main(String args[])
+    {
+        int a=10, b=10;
+        System.out.println("Subtraction : " + subtract(a,b));
+    }
+    int subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
